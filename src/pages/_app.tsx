5f1b5845c735d53438899fe6cf8f186162870bca
Felipe Natan <felipenatan@novaandradina.org>
2021-05-24 23:42:38 -0400

    add styled-component and global styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from 'styles/globals';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React com Next</title>
+        <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="apple-touch-icon" href="favicon.ico" />
+        <meta
+          name="description"
+          content="A simple project with react and NextJS"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
